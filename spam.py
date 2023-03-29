b3c55b0e42cabdeae192c06cff080479cc23b647
RFFreebander433 <129181357+RFFreebander433@users.noreply.github.com>
2023-03-29 12:29:54 +0200

    Add files via upload

---
+++ spam.py
@@ -0,0 +1,18 @@
+import pyautogui
+import time
+import subprocess
+import os
+import random
+
+print("Made by RFFreebander433")
+time.sleep(5)
+pyautogui.moveTo(457, 707) #moves the mouse to chatbox
+pyautogui.click()
+time.sleep(0.3)
+pyautogui.write('WELCOME TO CSMA OVERLOAD', interval=0.0)
+pyautogui.press('enter')
+
+
+
+
+
